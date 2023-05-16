cb57701656dbad424653d0625a5fd7629ab17c40
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-05-16 22:11:06 +0300

    Added arena allocator

---
+++ src/arena.c
@@ -0,0 +1,74 @@
+#include "arena.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+const struct mallocator_vtable MALLOCATOR_ARENA_VTABLE = {
+    .alloc   = (mallocator_alloc_t) mallocator_arena_alloc,
+    .realloc = NULL,
+    .free    = NULL
+};
+
+mallocator_arena_t mallocator_arena_mk(void *block, size_t size) {
+    assert(block);
+
+    return (mallocator_arena_t) {
+        .allocator = mallocator_mk(&MALLOCATOR_ARENA_VTABLE),
+        .block     = block,
+        .total     = size,
+        .used      = 0
+    };
+}
+
+mallocator_arena_t mallocator_arena_mk_alloc(size_t size, bool *failed) {
+    void *block = malloc(size);
+
+    if (!block && failed)
+        *failed = true;
+
+    return (mallocator_arena_t) {
+        .allocator = mallocator_mk(&MALLOCATOR_ARENA_VTABLE),
+        .block     = block,
+        .total     = size,
+        .used      = 0
+    };
+}
+
+void mallocator_arena_free(mallocator_arena_t *allocator) {
+    assert(mallocator_arena_valid(allocator));
+    free(allocator->block);
+}
+
+void *mallocator_arena_block(const mallocator_arena_t *allocator) {
+    assert(mallocator_arena_valid(allocator));
+    return allocator->block;
+}
+
+size_t mallocator_arena_total(const mallocator_arena_t *allocator) {
+    assert(mallocator_arena_valid(allocator));
+    return allocator->total;
+}
+
+size_t mallocator_arena_used(const mallocator_arena_t *allocator) {
+    assert(mallocator_arena_valid(allocator));
+    return allocator->used;
+}
+
+void *mallocator_arena_alloc(mallocator_arena_t *allocator, size_t size) {
+    assert(mallocator_arena_valid(allocator));
+
+    if (allocator->used + size > allocator->total)
+        return NULL;
+
+    void *block = allocator->block + allocator->used;
+
+    allocator->used += size;
+
+    return block;
+}
+
+bool mallocator_arena_valid(const mallocator_arena_t *allocator) {
+    return mallocator_valid((const mallocator_t *) allocator)
+        && allocator->block
+        && allocator->used <= allocator->total;
+}
---
+++ src/arena.h
@@ -0,0 +1,27 @@
+#ifndef FOMINMV_ALLOCATOR_ARENA_H
+#define FOMINMV_ALLOCATOR_ARENA_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "allocator.h"
+
+typedef struct {
+    mallocator_t  allocator;
+    void         *block;
+    size_t        total;
+    size_t        used;
+} mallocator_arena_t;
+
+extern const struct mallocator_vtable MALLOCATOR_ARENA_VTABLE;
+
+mallocator_arena_t mallocator_arena_mk(void *block, size_t size);
+mallocator_arena_t mallocator_arena_mk_alloc(size_t size, bool *failed);
+void mallocator_arena_free(mallocator_arena_t *allocator);
+void *mallocator_arena_block(const mallocator_arena_t *allocator);
+size_t mallocator_arena_total(const mallocator_arena_t *allocator);
+size_t mallocator_arena_used(const mallocator_arena_t *allocator);
+void *mallocator_arena_alloc(mallocator_arena_t *allocator, size_t size);
+bool mallocator_arena_valid(const mallocator_arena_t *allocator);
+
+#endif
